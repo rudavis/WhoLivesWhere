60f1d1b58c3e12a6553ad33d564455d9bcbfbb9e
Russ <rudavis@gmail.com>
2014-03-15 17:14:37 -0400

    refactored and clean up

--- javascripts/helpers.js
+++ javascripts/helpers.js
@@ -1,21 +1,44 @@
-
-//<fb:profile-pic uid= size="square" facebook-logo="true"></fb:profile-pic><fb:name uid=></fb:name>
-//<td><a href='http://www.facebook.com/"+response[i].uid+"' target='_blank'>"+response[i].name+"</a></td>
-function buildNameCell(u){
-    return "<td><a href='http://www.facebook.com/"+u.uid+"' target='_blank'><img src='"+ u.pic_square+ "'>&nbsp;&nbsp;" +u.name+"</a></td>";
-    //return "<td><p><fb:profile-pic uid='" + u.uid + "' size='square' facebook-logo='true'></fb:profile-pic>&nbsp;&nbsp;<fb:name uid='" + u.uid + "'></fb:name></p></td>";
+// Function to take user input and make it camel case
+//Apparently the FQL is case sensitive.  
+function toTitleCase(str) {
+    return str.replace(/(?:^|\s)\w/g, function(match) {
+        return match.toUpperCase();
+    });
 }
 
-//<td>"+  response[i].current_location.name +"</td>
+
+/**  Build the Name Cell **/
+/*  Sample 
+    <td>
+        <a href="http://www.facebook.com/1403289" target="_blank">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash3/t5.0-1/195548_1403289_5925109_q.jpg">
+            &nbsp;&nbsp;Nita Saetie Taylor
+        </a>
+    </td>
+*/
+function buildNameCell(u){
+    return "<td><a href='http://www.facebook.com/"+u.uid+"' target='_blank'><img src='"+ u.pic_square+ "'>&nbsp;&nbsp;" +u.name+"</a></td>";
+}
+
+/** Build the Location Cell */
+/* Sample
+    <td>Philadelphia, Pennsylvania</td>
+*/
 function buildLocationCell(u) {
     return "<td>" + u.current_location.name + "</td>";
 }
 
-//<td><button onClick=sendMessage("+ response[i].uid +") type='button' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-envelope'></span>&nbsp;&nbsp;Send Message</button></td>
+/** Build the Send message cell */
+/* Sample
+    <td>
+        <button onclick="sendMessage(1403289)" type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;Send Message</button>
+    </td>
+*/
 function buildSendMessageCell(u) {
     return "<td><button onClick=sendMessage("+ u.uid +") type='button' class='btn btn-default btn-sm'><span class='glyphicon glyphicon-envelope'></span>&nbsp;&nbsp;Send Message</button></td>";
 }
 
+/* Putting it all together */
 function buildRow(u) {
     return "<tr>" + buildNameCell(u) + buildLocationCell(u) + buildSendMessageCell(u) + "</tr>";
 }
